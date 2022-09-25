047f4389fd03aa42e84645b19a5c366f2770c9cf
Benjamin Kalatsjan-Kirejevas <bensujaam@gmail.com>
2022-09-25 15:27:25 +0200

    Added build and debug capabilities and also fixed the voxel shader

--- src/main.rs
+++ src/main.rs
@@ -29,19 +29,19 @@ fn run(event_loop_proxy: event_loop::EventLoopProxy) {
     let device_event_rx = event_loop_proxy.create_device_event_channel();
     let window_event_rx = event_loop_proxy.create_window_event_channel();
 
+    let window_builder = winit::window::WindowBuilder::new()
+        .with_title("Yet Another Minecraft Clone")
+        .with_inner_size(winit::dpi::LogicalSize::<f32>::new(1024.0, 768.0))
+        .with_resizable(false);
+
+    event_loop_proxy.create_window(window_builder).unwrap();
+
     let mut systems = Systems::new(
         PathBuf::from("res/"),
         &event_loop_proxy
     );
 
     {
-        let window_builder = winit::window::WindowBuilder::new()
-            .with_title("Yet Another Minecraft Clone")
-            .with_inner_size(winit::dpi::LogicalSize::<f32>::new(1024.0, 768.0))
-            .with_resizable(false);
-
-        event_loop_proxy.create_window(window_builder).unwrap();
-
         //The game loop
         let mut duration_behind: time::Duration = Default::default();
         let mut last_instant = time::Instant::now();
